40610b082c8b09873b47d940947a14ff4ccf2913
pj9898 <jprabhat65@gmail.com>
2020-04-12 20:23:32 +0530

    update upto 12.04.2020

---
+++ Edufox4/edufox/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class feedback(forms.Form):
+	comment=forms.CharField(max_length=30)
+	YourName=forms.CharField(max_length=30)
+	email=forms.EmailField(max_length=30)
+
+
+class register(forms.Form):
+	ContactNumber=forms.IntegerField(max_length=14)
+	YourName=forms.CharField(max_length=30)
+	email=forms.EmailField(max_length=30)
+	CourseName=forms.CharField(max_length=20)
